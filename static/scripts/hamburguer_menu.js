3dede60c02bda35be0fb10631844afa38d3a31b7
Edgar Lopez <edgarlopezmgs@live.com>
2021-03-07 23:04:30 -0600

    empezando el slider

--- static/scripts/hamburguer_menu.js
+++ static/scripts/hamburguer_menu.js
@@ -1,3 +1,21 @@
-function click () {
-    console.log("soy una funcion")
+
+function open_menu() {
+    const itemsMenuButton = document.getElementById("hamburguer-menu").children
+    const asideBar = document.getElementById("aside-bar")
+
+    const menuIsOpen = itemsMenuButton[0].classList.contains("hamburguer-menu__cancel")
+
+    if( !menuIsOpen ){
+        asideBar.className += " aside-bar__opened"
+    }else{
+        asideBar.classList.remove("aside-bar__opened")
+    }
+
+    for( let item of itemsMenuButton ){
+        if( menuIsOpen ){
+            item.classList.remove("hamburguer-menu__cancel")
+        }else{
+            item.className += " hamburguer-menu__cancel"
+        }
+    }
 }
---
+++ static/scripts/switch_theme.js
@@ -0,0 +1,20 @@
+if( localStorage.getItem("dark-mode") === "true" || window.matchMedia('(prefers-color-schema: dark') ) {
+    document.body.classList.add("dark")
+    document.getElementById("darkmode_icon").classList.add("switchtheme-circle__active")
+}else {
+    document.body.classList.remove("dark")
+    document.getElementById("darkmode_icon").classList.remove("switchtheme-circle__active")
+}
+
+function handleDarkMode(){
+    const themeIsDark = document.body.classList.toggle("dark")
+    const darkModeIcon = document.getElementById("darkmode_icon")
+    darkModeIcon.classList.toggle("switchtheme-circle__active")
+
+    if( themeIsDark ){
+        localStorage.setItem("dark-mode", `${"true"}`)
+    } else {
+        localStorage.setItem("dark-mode", `${"false"}`)
+    }
+}
+
